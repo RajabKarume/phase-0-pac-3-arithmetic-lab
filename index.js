feef403118c4cffc692df83cf031ea9e5bc72403
Rajab <rkarume@gmail.com>
2022-08-11 14:47:05 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,42 @@
+number = 50
+
+function add(){
+    return number += 1;
+}
+function subtract(){
+    return number -= 1;
+}
+function multiply(){
+    return number *= 10;
+}
+function divide(){
+    return number /= 3;
+}
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+n = 1;
+function increment(n) {
+    return n += 1;
+}
+function decrement(n) {
+    return n -= 1;
+}
+n = "20"
+function makeInt(n) {
+    n = Number.parseInt(n, 10);
+    return n ;
+}
+function preserveDecimal(n) {
+    n = parseFloat(n);
+    return n ;
+}
